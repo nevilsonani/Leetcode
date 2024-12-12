c1628169e6aedc7f47b94354383d234d2edbf4d1
Nevil Sonani <122978643+nevilsonani@users.noreply.github.com>
2024-12-12 08:51:22 +0530

    Time: 1003 ms (65.92%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT name, bonus
+FROM
+    Employee
+    LEFT JOIN Bonus USING (empId)
+WHERE IFNULL(bonus, 0) < 1000;
