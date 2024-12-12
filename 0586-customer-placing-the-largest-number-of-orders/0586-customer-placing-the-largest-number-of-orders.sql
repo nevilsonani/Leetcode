c3b61882441b21641469102cb92e4aa3fac645d7
Nevil Sonani <122978643+nevilsonani@users.noreply.github.com>
2024-12-12 08:56:03 +0530

    Time: 686 ms (13.35%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT
+    customer_number
+FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(1) DESC
+LIMIT 1;
