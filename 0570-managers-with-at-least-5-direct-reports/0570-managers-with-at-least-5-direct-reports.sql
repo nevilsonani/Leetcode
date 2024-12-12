13da717cfeaea3200c78e8c2c4bf299daf0fcca5
Nevil Sonani <122978643+nevilsonani@users.noreply.github.com>
2024-12-12 08:46:59 +0530

    Time: 654 ms (8.2%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT name
+FROM
+    Employee
+    JOIN (
+        SELECT managerId AS id, COUNT(1) AS cnt
+        FROM Employee
+        GROUP BY 1
+        HAVING cnt >= 5
+    ) AS t
+        USING (id);
